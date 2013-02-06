2255a0f7e68f499a0b4ae108c2322107f60a0502
endtojunc <endtojunc.dev@gmail.com>
2013-02-06 16:54:28 +0800

    sendmail.php

--- php/sendemail.php
+++ php/sendemail.php
@@ -1,10 +1,22 @@
 <?php
 
-$mailTo = "contact_us@pandaeyes.net"
-$mailFrom = $_POST['email'];
+$to = "contact_us@pandaeyes.net"
+$from = $_POST['email'];
 $subject = "Pandaeyes Contact from client"
-$message = $_POST['message'];
-
-			
-mail($mailTo, $subject, $message, "From: ".$mailFrom);
+$body = $_POST['message'];
+ 
+ $host = "mail.pandaeyes.net";
+ $username = "contact_us@pandaeyes.net";
+ $password = "p@nd@eyes325";
+ 
+ $headers = array ('From' => $from,
+   'To' => $to,
+   'Subject' => $subject);
+ $smtp = Mail::factory('smtp',
+   array ('host' => $host,
+     'auth' => true,
+     'username' => $username,
+     'password' => $password));
+ 
+ $mail = $smtp->send($to, $headers, $body);
 ?>
