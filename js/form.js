f096157437941d3032a3cdc9a28ce9a7dd450353
shinn <shinn@tinkerbox.com.sg>
2013-02-23 15:16:44 +0800

    update email regex & exclaimation mark's position

--- js/form.js
+++ js/form.js
@@ -10,31 +10,31 @@ $(document).ready(function() {
 function validateForm()
 {
 	$('.error').hide();
-	var emailReg = "[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}";
+	var emailReg = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
 
 	var emailToVal = $("#email").val();
 	if(emailToVal == '')
 	{
-		$("#email").siblings('label').before('<img src="img/error.png" class="error"/>');
+		$("#email").after('<img src="img/error.png" class="error"/>');
 		hasError = true;
 	}
 	else if(!emailReg.test(emailToVal))
 	{
-		$("#email").siblings('label').before('<img src="img/error.png" class="error"/>');
+		$("#email").after('<img src="img/error.png" class="error"/>');
 		hasError = true;
 	}
 
 	var name = $("#name").val();
 	if(name == "")
 	{
-		$("#name").siblings('label').before('<img src="img/error.png" class="error"/>');
+		$("#name").after('<img src="img/error.png" class="error"/>');
 		hasError = true;
 	}
 
 	var message = $("#message").val();
 	if(message == "")
 	{
-		$("#message").siblings('label').before('<img src="img/error.png" class="error"/>');
+		$("#message").after('<img src="img/error.png" class="error"/>');
 		hasError = true;
 	}
 
