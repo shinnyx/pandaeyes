3c7356f140839b5cbdfc79a86ac8cd506ea94fc5
shinn <shinn@tinkerbox.com.sg>
2013-02-23 13:43:40 +0800

    fixed form validation issue & moved inline js to script.js

--- js/form.js
+++ js/form.js
@@ -1,32 +1,40 @@
+$(document).ready(function() {
+	var hasError = false;
+	$.each([$('#name'), $('#email'), $('#message')], function() {
+		$(this).focus(function() {
+			$(this).siblings(".error").remove();
+		});
+	});
+})
+
 function validateForm()
 {
-	$(".error").hide();
-	var hasError = false;
+	$('.error').hide();
 	var emailReg = "[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}";
 
 	var emailToVal = $("#email").val();
 	if(emailToVal == '')
 	{
-		$("#email").before('<img src="img/error.png" class="error"/>');
+		$("#email").siblings('label').before('<img src="img/error.png" class="error"/>');
 		hasError = true;
 	}
 	else if(!emailReg.test(emailToVal))
 	{
-		$("#email").before('<img src="img/error.png" class="error"/>');
+		$("#email").siblings('label').before('<img src="img/error.png" class="error"/>');
 		hasError = true;
 	}
 
 	var name = $("#name").val();
 	if(name == "")
 	{
-		$("#name").before('<img src="img/error.png" class="error"/>');
+		$("#name").siblings('label').before('<img src="img/error.png" class="error"/>');
 		hasError = true;
 	}
 
 	var message = $("#message").val();
 	if(message == "")
 	{
-		$("#message").before('<img src="img/error.png" class="error"/>');
+		$("#message").siblings('label').before('<img src="img/error.png" class="error"/>');
 		hasError = true;
 	}
 
---
+++ js/script.js
@@ -0,0 +1,94 @@
+<!-- toggle text change -->
+$(function() {
+	var toggle = $('#slidingArrow');
+	toggle.mousedown(function() {
+		$(this).parent().parent().siblings("#slidingBarContent").slideToggle('1000', function() {
+			toggle.text($(this).is(':visible') ? "▲" : "▼");
+		});
+	});
+});
+
+<!-- carousel -->
+jQuery(function($) {
+	function generatePages() {
+		var _total, i, _link;
+		_total = $( "#carousel" ).rcarousel( "getTotalPages" );
+		
+		for ( i = 0; i < _total; i++ ) {
+			_link = $( "<a href='#'></a>" );
+			
+			$(_link)
+				.bind("click", {page: i},
+					function( event ) {
+						$( "#carousel" ).rcarousel( "goToPage", event.data.page );
+						event.preventDefault();
+					}
+				)
+				.addClass( "bullet off" )
+				.appendTo( "#pages" );
+		}
+		
+		// mark first page as active
+		$( "a:eq(0)", "#pages" )
+			.removeClass( "off" )
+			.addClass( "on" )
+			.html('<div class="bullet"><div class="dot"></div></div>');
+	}
+
+	function pageLoaded( event, data ) {
+		$( "a.on", "#pages" )
+			.removeClass( "on" )
+			.html('<div class="bullet"></div>');
+
+		$( "a", "#pages" )
+			.eq( data.page )
+			.addClass( "on" )
+			.html('<div class="bullet"><div class="dot"></div></div>');
+			
+		$("#weBuild").addClass("bounceInRight");
+	}
+	
+	$("#carousel").rcarousel(
+		{
+			visible: 1,
+			step: 1,
+			speed: 700,
+			auto: {
+				enabled: true
+			},
+			width: 960,
+			height: 400,
+			start: generatePages,
+			pageLoaded: pageLoaded
+		}
+	);
+	
+	$( "#ui-carousel-next" )
+		.add( "#ui-carousel-prev" )
+		.add( ".bullet" )
+		.hover(
+			function() {
+				$( this ).css( "opacity", 0.7 );
+			},
+			function() {
+				$( this ).css( "opacity", 1.0 );
+			}
+		);
+});
+
+<!-- animated scrolling -->
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var $target = $(this.hash), target = this.hash;
+    	var targetOffset = $target.offset().top;
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: targetOffset
+            }, 1000, function() { location.hash = target; });
+            return false;
+        }
+    }
+});
